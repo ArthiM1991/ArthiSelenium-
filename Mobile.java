0dfb84d2fb25e2f62fd6ece446cde401aa9fa141
ArthiM1991 <152076333+ArthiM1991@users.noreply.github.com>
2023-11-27 21:23:26 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,27 @@
+package week2.day1;
+
+public class Mobile {
+	
+	public void makeCall()
+	{
+		String mobileModel="Redmi";
+		float mobileWeight=12f;
+		System.out.println("Mobile model is "+mobileModel+"\n"+"Mobile weight is "+mobileWeight);
+	}
+	
+	public void sendMsg()
+	{
+		boolean isFullCharged= true;
+		int mobileCost=15000;
+		
+		System.out.println(isFullCharged+"\n"+"mobile cost is "+mobileCost);
+	}
+
+	public static void main(String[] args) {
+		
+		Mobile mob=new Mobile();
+		mob.makeCall();
+		mob.sendMsg();
+		System.out.println("This is my mobile");
+	}
+}
